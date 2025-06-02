314b75cf14cb0d5f2b0f92ed4f4bba9591833ac8
Csontos Kincső Anasztázia <91425554+doomhyena@users.noreply.github.com>
2025-06-02 13:47:34 +0200

    Dolgozat feladatok

---
+++ 2024-25-13-evfolyam/asztali-alkalmazasok-I-info-alapok-II/feladat.php
@@ -0,0 +1,90 @@
+
+	A feladatok medoldását az adott feladat szövege alá írd!
+	Ha egy feladat parancsot kér, akkor a zárójelek is kellenek!
+
+	Neved:
+	
+	1. Készíts egy for ciklust. Az i változó 1-től 10-ig vegyen fel értéket.
+	
+	
+	
+	2. Session elindításának parancsa.
+	
+	
+	
+	3. Hozz létre egy session változót userid néven, és add meg neki a 3-as értéket!
+	
+	
+	
+	4. Mit kell megadni egy <form> tagbe, hogy adatokat tudjunk kinyerni az űrlapból?
+	
+	
+	
+	5. Hogyan tudjuk leellenőrizni, hogy az űrlapunk "login-btn" gombja meg lett-e nyomva?
+	
+	
+	
+	6. Jelszó titkosítása (jelszó: 123):
+	
+	
+	
+	7. Titkosított jelszó ellenőrzése:
+	
+	
+	
+	8. Hozz létre egy sütit "id" néven, és add meg neki a 4-es értéket! A süti élettartama 2 óra legyen.
+	
+	
+	
+	9. Javascript intervallum létrehozása:
+	
+	
+	
+	10. Javascript adat kinyerése egy input boxból gépelés alapján:
+	
+	
+	
+	11. loadnames PHP fájl behívása javascripttel egy "talalatok" id-val ellátott div-be.
+	
+	
+	
+	12. Config cájl
+	
+	
+	
+	13. Írj egy teljes lekérdezést. A lekérdezés a users táblát használja, és az 5-ös id-val ellátott felhasználót keresse. A lekérdezés végén tárold el a felhasználó adatait egy "felhasznalo" változóba!
+	
+	
+	
+	14. Írj egy teljes lekérdezést. A lekérdezés a posts táblát használja, és az összes olyan sort kérdezze le, ahol a userid 3. A találatokon egyesével menjen végig a lekérdezés, és minden talált sor adatát tárolja el egy $sor változóban (feébontva).
+	
+	
+	15. Talált sorok megszámolására használt parancs.
+	
+	
+	
+	16. Mi az általunk használt 4 adatbázis művelet?
+	
+	
+	
+	Innentől csak SQL-t kell írnod.
+	
+	
+	
+	17. Töröld azt a sort az orders táblából, ahol az orderNumber oszlop értéke 1685345!
+	
+	
+	
+	18. Kérdezd le az első 5 játékost a scores táblából elért pontok (points) alapján csökkenő sorrendben!
+	
+	
+	
+	19. Módosítsd az adatbázisban a 13-as id-val rendelkező felhasználó email címét "test@test.com"-ra!
+	
+	
+	
+	20. Töltsd fel a locations táblába a következő adatokat:
+	
+		- name: Schola
+		- street: Murányi
+		- streetNumber: 10
