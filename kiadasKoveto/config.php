b04e7c9502b09e5fb22f136c90970196cfe7e85e
Csontos Kincső Anasztázia <csontoskincso@proton.me>
2026-05-25 20:01:35 +0200

    Dupblikált anyagok törölve

--- kiadasKoveto/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","kiadasok");
-if (mysqli_connect_errno())
-  {
-  echo "Sikertelen MySQL csatlakozás: " . mysqli_connect_error() ." | Hiányzó adatbázis.";
-  }
-?>
